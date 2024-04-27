4229a4f06b5d220524c1360719b7ee9dd47bf89c
mustafa sen <mustafasen@tuta.io>
2024-04-27 22:43:57 +0300

    added parser and points

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import index from "./routes/index.js";
 import point from "./routes/point.js";
 import initializePointStore from "./utils/init-point-store.js";
+import bodyParser from "body-parser";
 
 (async function main() {
   console.log(await initializePointStore());
   const app = express();
+  app.use(bodyParser.json());
   app.use(index);
-  app.use("/point", point);
+  app.use("/points", point);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
