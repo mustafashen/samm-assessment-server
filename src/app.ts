3dedcc56ee3566ce2d1f0aca5d20dac71b0555c4
mustafa sen <mustafasen@tuta.io>
2024-04-28 03:09:24 +0300

    added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import index from "./routes/index.js";
 import points from "./routes/points.js";
 import initializePointStore from "./utils/init-point-store.js";
 import bodyParser from "body-parser";
+import cors from "cors"
 
 (async function main() {
   console.log(await initializePointStore());
   const app = express();
+  app.use(cors())
   app.use(bodyParser.json());
   app.use(index);
   app.use("/points", points);
